c3a09c9ca4fde6c7cf6609db065de8179087ce79
Jerry Kuch <jerrykuch@gmail.com>
2024-02-06 08:24:44 -0800

    Improve cqlsh.sh Argument Handling

--- cqlsh.sh
+++ cqlsh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $# -eq 0 ];
-then echo "Usage:  cqlsh.sh node_container_name"
+then echo "Usage:  cqlsh.sh node_container_name [other_args_to_cqlsh]"
      exit
 fi
 
-docker exec -it $1 cqlsh
+docker exec -it $1 cqlsh ${@:2}
 
 
